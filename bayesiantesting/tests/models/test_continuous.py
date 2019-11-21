c97d02322c97bb0107fdc26e9a91088cc7cb68fc
SimonBoothroyd <simon.boothroyd@colorado.edu>
2019-11-21 14:16:02 -0700

    Removes unused test.

--- bayesiantesting/tests/models/test_continuous.py
+++ bayesiantesting/tests/models/test_continuous.py
@@ -120,25 +120,3 @@ def test_evaluate_log_posterior(model):
 
     assert len(prior_gradients) == len(parameters)
     assert not numpy.allclose(prior_gradients, 0.0)
-
-
-# def test_gradient_speed():
-#
-#     model = _get_two_center_model()
-#
-#     mu = numpy.array([94.8, 0.353, 0.120, 0.0])
-#     sigma = mu / 50.0
-#
-#     prior_gradient_function = autograd.grad(model.evaluate_log_posterior)
-#     n_iter = 1000
-#
-#     start = perf_counter_ns()
-#
-#     for i in range(n_iter):
-#         parameters = numpy.random.normal(mu, sigma)
-#         prior_gradient_function(parameters)
-#
-#     stop = perf_counter_ns()
-#
-#     time = ((stop - start) / n_iter) * 1e-9
-#     print(time)
