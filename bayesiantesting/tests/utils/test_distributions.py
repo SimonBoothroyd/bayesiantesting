b6d644a35d3c02b9d81c644a0b1bf92399e234d2
SimonBoothroyd <simon.boothroyd@colorado.edu>
2019-11-21 10:23:08 -0700

    black / flake8.

--- bayesiantesting/tests/utils/test_distributions.py
+++ bayesiantesting/tests/utils/test_distributions.py
@@ -31,14 +31,17 @@ def test_values(distribution_type, reference_type, args):
     assert numpy.isclose(
         distribution.cdf(sample),
         reference.cdf(torch.tensor(sample, dtype=torch.float64)),
+        rtol=1.0e-2,
     )
     assert numpy.isclose(
         distribution.inverse_cdf(cdf_sample),
         reference.icdf(torch.tensor(cdf_sample, dtype=torch.float64)),
+        rtol=1.0e-2,
     )
     assert numpy.isclose(
         distribution.log_pdf(sample),
         reference.log_prob(torch.tensor(sample, dtype=torch.float64)),
+        rtol=1.0e-2,
     )
 
 
@@ -69,16 +72,16 @@ def test_gradients(distribution_type, reference_type, args):
     sample_tensor = torch.tensor(sample, requires_grad=True, dtype=torch.float64)
     reference_cdf = reference.cdf(sample_tensor)
     reference_cdf.backward()
-    assert numpy.isclose(cdf_gradient, sample_tensor.grad.item())
+    assert numpy.isclose(cdf_gradient, sample_tensor.grad.item(), rtol=1.0e-2)
 
     sample_tensor = torch.tensor(cdf_sample, requires_grad=True, dtype=torch.float64)
     reference_inverse_cdf = reference.icdf(sample_tensor)
     reference_inverse_cdf.backward()
-    assert numpy.isclose(inverse_cdf_gradient, sample_tensor.grad.item())
+    assert numpy.isclose(inverse_cdf_gradient, sample_tensor.grad.item(), rtol=1.0e-2)
 
     sample_tensor = torch.tensor(sample, requires_grad=True, dtype=torch.float64)
     reference_log_p = reference.log_prob(sample_tensor)
 
     if reference_log_p._grad_fn is not None:
         reference_log_p.backward()
-        assert numpy.isclose(log_p_gradient, sample_tensor.grad.item())
+        assert numpy.isclose(log_p_gradient, sample_tensor.grad.item(), rtol=1.0e-2)
