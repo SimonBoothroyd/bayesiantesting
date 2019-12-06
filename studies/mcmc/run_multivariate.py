2ed2bc95cd2540da617aa4c6c6605c6c8b3d15c0
SimonBoothroyd <simon.boothroyd@colorado.edu>
2019-12-06 13:48:43 -0700

    Cleans up the multivariate study. Makes the HMC sampler multivariate compatible.

--- studies/mcmc/run_multivariate.py
+++ studies/mcmc/run_multivariate.py
@@ -4,55 +4,36 @@ import numpy
 
 from bayesiantesting.kernels import MCMCSimulation
 from bayesiantesting.models.continuous import UnconditionedModel
-from studies.utilities import parse_input_yaml
 
 
 def main():
 
-    simulation_params = parse_input_yaml("basic_run.yaml")
+    # Set up an unconditoned model with a combined random
+    # multivariate normal / half normal prior.
+    mean = numpy.random.rand(3)
 
-    # Set some initial parameter close to the MAP taken from
-    # other runs of C2H6.
-    initial_parameters = numpy.array([140.0, 0.35, 0.26, 0.01])
+    covariance = numpy.random.rand(3, 3)
+    covariance = numpy.dot(covariance, covariance.T)
 
     prior_settings = {
-        ("epsilon", "sigma", "L"): [
-            "multivariate normal",
-            [
-                numpy.array([122.9351, 0.3581, 0.2068]),
-                numpy.array(
-                    [
-                        [
-                            5.514607656954375,
-                            -0.003907597860443889,
-                            0.011927926543293587,
-                        ],
-                        [
-                            -0.003907597860443889,
-                            2.7821697309014395e-06,
-                            -8.46201728756609e-06,
-                        ],
-                        [
-                            0.011927926543293587,
-                            -8.46201728756609e-06,
-                            2.581822857777358e-05,
-                        ],
-                    ]
-                ),
-            ],
-        ],
-        "Q": ["half normal", [0.01]],
+        ("a", "b", "c"): ["multivariate normal", [mean, covariance]],
+        "d": ["half normal", [0.01]],
     }
 
+    initial_parameters = numpy.array([*mean, 0.01])
+
     # Build the model / models.
-    model = UnconditionedModel("multivariate", prior_settings)
+    model = UnconditionedModel("multivariate", prior_settings, {})
 
     # Run the simulation.
+    sampler = None  # NUTS(None, ModelCollection(model.name, [model]), 1.0)
+
     simulation = MCMCSimulation(
         model_collection=model,
-        warm_up_steps=int(simulation_params["steps"] * 0.2),
-        steps=simulation_params["steps"],
+        warm_up_steps=100000,
+        steps=1500000,
         discard_warm_up_data=True,
+        sampler=sampler
     )
 
     trace, log_p_trace, percent_deviation_trace = simulation.run(initial_parameters)
